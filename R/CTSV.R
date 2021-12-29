0867afe2d5106610f05847f0c7664791a86631ed
JingeYu <65295370+jingeyu@users.noreply.github.com>
2021-12-29 17:02:38 +0800

    Add files via upload

--- R/CTSV.R
+++ R/CTSV.R
@@ -78,7 +78,7 @@ ctsv <- function(Y, loc, W, num_core=1){
     # Center and normalize coordinates of spots to have mean zero and standard deviation one.
     S <- t(loc) - colMeans(loc)
     S <- t(S / apply(S, 1, sd))
-    quan <- c(0.2,0.4,0.6,0.8,1.0)
+    quan <- c(0.4,0.6)
     psi1 <- quantile(abs(S[,1]), quan)
     psi2 <- quantile(abs(S[,2]), quan)
     P_VAL <- array(NA, dim = c(G,2*K,5))
@@ -92,17 +92,17 @@ ctsv <- function(Y, loc, W, num_core=1){
     pattern <- c("linear","gau1","gau2","cos1","cos2")
     for(fit_pat in pattern){
         if(fit_pat == "gau1"){
+            h1 <- exp(-S[,1]^2 / 2 / psi1[1]^2)
+            h2 <- exp(-S[,2]^2 / 2 / psi2[1]^2)
+        }else if(fit_pat == "gau2"){
             h1 <- exp(-S[,1]^2 / 2 / psi1[2]^2)
             h2 <- exp(-S[,2]^2 / 2 / psi2[2]^2)
-        }else if(fit_pat == "gau2"){
-            h1 <- exp(-S[,1]^2 / 2 / psi1[3]^2)
-            h2 <- exp(-S[,2]^2 / 2 / psi2[3]^2)
         }else if(fit_pat == "cos1"){
+            h1 <- cos(2*pi*S[,1] / psi1[1])
+            h2 <- cos(2*pi*S[,2] / psi2[1])
+        } else if(fit_pat == "cos2"){
             h1 <- cos(2*pi*S[,1] / psi1[2])
             h2 <- cos(2*pi*S[,2] / psi2[2])
-        } else if(fit_pat == "cos2"){
-            h1 <- cos(2*pi*S[,1] / psi1[3])
-            h2 <- cos(2*pi*S[,2] / psi2[3])
         }else{
             h1 <- S[,1]
             h2 <- S[,2]
@@ -126,7 +126,7 @@ ctsv <- function(Y, loc, W, num_core=1){
             } else{
                 p_val <- coef(summary(fm_zinb0))$count[,4]
                 nind <- 2*(length(p_val) - 1)/3
-                p_val <- p_val[1:nind]
+                p_val <- p_val[1:(2*K)]
                 
             }
             return(p_val)
